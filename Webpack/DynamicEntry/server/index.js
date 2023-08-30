07227e44b89cc7013af9008872652e28377435e3
MaXianZhang <zhangxu6271@163.com>
2023-08-30 21:26:03 +0800

    动态entry

--- Webpack/DynamicEntry/server/index.js
+++ Webpack/DynamicEntry/server/index.js
@@ -1,26 +1,24 @@
 const webpack = require('webpack');
 const webpackConfig = require('../webpack.config.js')
 const SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin');
-const path = require('path');
-
-
-console.log(2323, process.cwd())
 
 const compiler = webpack(webpackConfig);
+compiler.run();
 
-function addNewWidgetEntry(widgetName, entryPath) {
+function addNewWidgetEntry(fileName, entryPath) {
     return new Promise(resolve => {
-        console.log(23234)
+        console.log(23234, process.cwd(), entryPath, fileName)
         new SingleEntryPlugin(
             process.cwd(),
             entryPath,
-            widgetName
+            fileName
         ).apply(compiler);
     });
 }
 
 setTimeout(() => {
-    addNewWidgetEntry('entry2', path.resolve(__dirname, '../src/entry-2.js'));
+    addNewWidgetEntry('entry2', './src/entry-2');
+    compiler.run();
 }, 3000)
 
 
