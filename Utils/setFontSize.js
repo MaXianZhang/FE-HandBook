c9ec636887fafa7521ace0bb70cab7764bb5feb3
MaXianZhang <zhangxu6271@163.com>
2023-09-02 23:38:06 +0800

    setFontSize

---
+++ Utils/setFontSize.js
@@ -0,0 +1,26 @@
+/** 给 h5 html设置font-size
+ *
+ * @param pageConfig
+ * @param isEditor 是否是编辑器环境
+ */
+
+// 默认的H5页面的宽度
+const DEFAULT_H5_PAGE_WIDTH = 375;
+// 默认的字体大小
+const DEFAULT_H5_FONT_SIZE = 16;
+
+
+function setFontSize(pageConfig, isEditor) {
+    if (!pageConfig.isH5Page) {
+        return;
+    }
+    const html = document.getElementsByTagName('html')[0];
+    if (!html) {
+        return;
+    }
+    currentPageWidth = document.body.clientWidth;
+    const h5BaseFontSize = isEditor
+        ? DEFAULT_H5_FONT_SIZE
+        : currentPageWidth / (DEFAULT_H5_PAGE_WIDTH / DEFAULT_H5_FONT_SIZE);
+    html && html.style && html.style.setProperty('font-size', `${h5BaseFontSize}px`);
+};
